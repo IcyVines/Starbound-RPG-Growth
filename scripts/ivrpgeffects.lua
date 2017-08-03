450c414956312bb9667b7cd02eb987b6cc9592fa
Roy <IcyVines@users.noreply.github.com>
2017-08-02 21:15:40 -0400

    fixed literal game-breaking bug

---
+++ scripts/ivrpgeffects.lua
@@ -0,0 +1,17 @@
+require("/scripts/raceEffects.lua")
+local fuoldInit = init
+local fuoldUpdate = update
+local fuoldUninit = uninit
+
+function init()
+  fuoldInit()
+  self.lastYPosition = 0
+  self.lastYVelocity = 0
+  self.fallDistance = 0
+  local bounds = mcontroller.boundBox() --Mcontroller for movement
+end
+
+function update(dt)
+  fuoldUpdate(dt)
+  status.addEphemeralEffect("ivrpgstatboosts",math.huge)
+end
