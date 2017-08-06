3a1124016137200207694d2f0c72003578a05bef
Roy <IcyVines@users.noreply.github.com>
2017-08-06 05:31:29 -0400

    slightly working slam

--- tech/knighttech/knightslam/knightslam.lua
+++ tech/knighttech/knightslam/knightslam.lua
@@ -1,47 +1,65 @@
-function init()
-  self.multiJumpCount = config.getParameter("multiJumpCount")
-  self.multiJumpModifier = config.getParameter("multiJumpModifier")
-  self.cost = config.getParameter("cost")
+require "/scripts/keybinds.lua"
 
+function init()
+  self.jumpsLeft = config.getParameter("multiJumpCount")
+  self.cost = config.getParameter("cost")
+  self.downPressed = false
+  self.jumped = false
+  self.lastYVelocity = 0
   refreshJumps()
+
+  Bind.create({jumping = true, onGround = false, liquidPercentage = 0}, doMultiJump)
+  Bind.create({down = true, onGround = false, liquidPercentage = 0}, doSlam)
 end
 
 function update(args)
-  local jumpActivated = args.moves["jump"] and not self.lastJump
-  self.lastJump = args.moves["jump"]
 
-  updateJumpModifier()
-
-  if jumpActivated and canMultiJump() then
-    if status.overConsumeResource("energy", self.cost) then
-      doMultiJump()
-    end
-  else
-    if mcontroller.groundMovement() or mcontroller.liquidMovement() or status.resource("energy") < 1 then
-      status.removeEphemeralEffect("camouflage25")
-      status.removeEphemeralEffect("invulnerable")
-      status.removeEphemeralEffect("nofalldamage")
-      refreshJumps()
-    end
+  if mcontroller.falling() then
+    self.lastYVelocity = mcontroller.yVelocity()
   end
+
+  if mcontroller.onGround() and self.downPressed and not self.jumped then
+    self.downPressed = false
+    --sb.logInfo("Last Y"..tostring(self.lastYVelocity))
+    status.removeEphemeralEffect("nofalldamage")
+    local damageConfig = {
+      power = math.abs(self.lastYVelocity)*2,
+      speed = 0,
+      physics = "default"
+    }
+    world.spawnProjectile("armorthornburst", mcontroller.position(), entity.id(), {0, 0}, true, damageConfig)
+  end
+
+  if mcontroller.groundMovement() or mcontroller.liquidMovement() then
+      refreshJumps()
+  end
+
 end
 
--- after the original ground jump has finished, start applying the new jump modifier
-function updateJumpModifier()
-  if self.multiJumpModifier then
-    if not self.applyJumpModifier
-        and not mcontroller.jumping()
-        and not mcontroller.groundMovement() then
+function doSlam()
+  --set flashjump player changes
+  self.downPressed = true
+  status.addEphemeralEffect("nofalldamage", math.huge)
+  mcontroller.controlJump(true)
+  mcontroller.setYVelocity(math.min(-1, mcontroller.yVelocity())*20)
+  animator.burstParticleEmitter("jumpParticles")
+end
 
-      self.applyJumpModifier = true
-    end
-
-    if self.applyJumpModifier then mcontroller.controlModifiers({airJumpModifier = self.multiJumpModifier}) end
+function doMultiJump()
+  if not canMultiJump() then
+    return
   end
+  self.downPressed = false
+  self.jumped = true
+  mcontroller.controlJump(true)
+  mcontroller.setYVelocity(math.max(0, mcontroller.yVelocity()))
+  animator.burstParticleEmitter("jumpParticles")
+  self.jumpsLeft = self.jumpsLeft - 1
+  animator.playSound("multiJumpSound")
 end
 
 function canMultiJump()
-  return self.multiJumps > 0
+  return self.jumpsLeft > 0
       and not mcontroller.jumping()
       and not mcontroller.canJump()
       and not mcontroller.liquidMovement()
@@ -49,29 +67,7 @@ function canMultiJump()
       and math.abs(world.gravity(mcontroller.position())) > 0
 end
 
-function doMultiJump()
-  --set flashjump player changes
-
-  status.addEphemeralEffect("camouflage25", 20)
-  status.addEphemeralEffect("invulnerable", 20)
-  status.addEphemeralEffect("nofalldamage", 20)
-
-  mcontroller.controlJump(true)
-  mcontroller.setYVelocity(math.max(0, mcontroller.yVelocity()))
-  self.facing = mcontroller.facingDirection()
-  --self.facing = tech.aimPosition()[1]-mcontroller.position()[1]
-  if self.facing < 0 then
-    mcontroller.setXVelocity(-50 + math.min(0, mcontroller.xVelocity()))
-    animator.burstParticleEmitter("jumpParticles")
-  else
-    mcontroller.setXVelocity(50 + math.max(0, mcontroller.xVelocity()))
-    animator.burstParticleEmitter("jumpParticles")
-  end
-  self.multiJumps = self.multiJumps - 1
-  animator.playSound("multiJumpSound")
-end
-
 function refreshJumps()
-  self.multiJumps = self.multiJumpCount
-  self.applyJumpModifier = false
+  self.jumped = false
+  self.jumpsLeft = config.getParameter("multiJumpCount")
 end
