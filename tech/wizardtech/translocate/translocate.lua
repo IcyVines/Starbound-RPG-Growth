37e36c8b382ab3ae80f0f0b2d2cba1562a8c6276
DanManN <dnahimov@gmail.com>
2017-08-07 19:08:46 -0400

    works

--- tech/wizardtech/translocate/translocate.lua
+++ tech/wizardtech/translocate/translocate.lua
@@ -1,29 +1,32 @@
+require "/scripts/keybinds.lua"
+
 function init()
   self.dashCooldownTimer = 0
   self.rechargeEffectTimer = 0
 
   self.dashMaxDistance = config.getParameter("dashDistance")
   self.dashCooldown = config.getParameter("dashCooldown")
+  sb.logInfo("ConfigCooldown: " .. tostring(self.dashCooldown))
   self.rechargeDirectives = config.getParameter("rechargeDirectives", "?fade=CCCCFFFF=0.25")
   self.rechargeEffectTime = config.getParameter("rechargeEffectTime", 0.1)
 
-  Bind.create("f", translocate)
-
+  Bind.create("Up", translocate)
 end
 
 function translocate()
+  sb.logInfo("Cooldown: " .. tostring(self.dashCooldownTimer))
   if self.dashCooldownTimer == 0 then
     local projectileId = world.spawnProjectile(
         "translocatordisc",
         tech.aimPosition(),
         entity.id(),
-        {0,0},
+        {0,1},
         false,
-        {}
+        {speed = 10}
       )
+    sb.logInfo("projectile created: " .. tostring(projectileId)) 
     if projectileId then
-      storage.projectileId = projectileId
-      world.callScriptedEntity(projectileId, "setOwnerId", activeItem.ownerEntityId())
+      world.callScriptedEntity(projectileId, "setOwnerId", entity.id())
       status.setStatusProperty("translocatorDiscId", projectileId)
       status.addEphemeralEffect("translocate")
       self.dashCooldownTimer = self.dashCooldown
