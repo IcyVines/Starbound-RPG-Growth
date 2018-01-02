db314cf31dfb8016984c7c1b1a2cccb584a2cd99
Roy <IcyVines@users.noreply.github.com>
2018-01-02 00:06:47 -0500

    Added appropriate grenade images. Changed Affinity trail lighting to be dimmer (less intrusive). Added animation and sound to Shock Charge. Added animation and proper sticking to tripmine.

--- projectiles/grenades/tripmine/tripmine.lua
+++ projectiles/grenades/tripmine/tripmine.lua
@@ -1,16 +1,39 @@
 require "/scripts/vec2.lua"
+require "/scripts/poly.lua"
 
 function init()
   self.queryDelta = 10
   self.queryStep = self.queryDelta
+  self.id = entity.id()
+  self.stuckDirection = nil
 end
 
 function update(dt)
-  if self.hitGround or mcontroller.onGround() then
+
+  if mcontroller.onGround() then
     mcontroller.setRotation(0)
-    self.hitGround = true
-  else
-    mcontroller.setRotation(math.atan(mcontroller.velocity()[2], mcontroller.velocity()[1]))
+    --self.hitGround = true
+  elseif self.stuckDirection then
+    if self.stuckDirection == "left" then 
+      mcontroller.setRotation(-math.pi/2)
+      mcontroller.setXVelocity(-10)
+    elseif self.stuckDirection == "right" then 
+      mcontroller.setRotation(math.pi/2)
+      mcontroller.setXVelocity(10)
+    elseif self.stuckDirection == "top" then 
+      mcontroller.setRotation(math.pi)
+      mcontroller.setYVelocity(10)
+    end
+  end
+
+  for i=-1,1,2 do
+    if world.lineTileCollision(mcontroller.position(), {mcontroller.xPosition(), mcontroller.yPosition() + i*0.5}) then
+      self.stuckDirection = i == 1 and "top" or self.stuckDirection
+      break
+    elseif world.lineTileCollision(mcontroller.position(), {mcontroller.xPosition() + i*0.4, mcontroller.yPosition()}) then
+      self.stuckDirection = i == -1 and "left" or "right"
+      break
+    end
   end
 
   self.queryStep = math.max(0, self.queryStep - 1)
@@ -23,4 +46,5 @@ function update(dt)
     end
     self.queryStep = self.queryDelta
   end
+
 end
