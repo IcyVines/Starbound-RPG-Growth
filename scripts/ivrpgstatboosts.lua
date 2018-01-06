10ac06ba746ce530116b6bf09163b230cf341aec
Roy <IcyVines@users.noreply.github.com>
2018-01-05 21:13:48 -0500

    XP Pulse

--- scripts/ivrpgstatboosts.lua
+++ scripts/ivrpgstatboosts.lua
@@ -896,7 +896,7 @@ function updateXPPulse()
 			status.clearPersistentEffects("ivrpgmultiplayerxp")
 		else
 			world.playerQuery(mcontroller.position(), 10, {
-				callScript = "addCurrency",
+				callScript = "giveItem",
          		callScriptArgs = {"experienceorb", world.entityCurrency(self.id, "experienceorb") - self.xp}
          	})
 			--world.spawnProjectile("multiplayerxppulse", mcontroller.position(), self.id, {0,0}, false, {power = 0, knockback = 0, timeToLive = 0.1, statusEffects = {{effect = "multiplayerxppulse", duration = world.entityCurrency(self.id, "experienceorb") - self.xp}}})
