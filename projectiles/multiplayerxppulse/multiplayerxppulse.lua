dffddda926bd82c8ce9fc52be50d4571991603de
Roy <IcyVines@users.noreply.github.com>
2018-01-06 00:16:31 -0500

    XP Pulse

--- projectiles/multiplayerxppulse/multiplayerxppulse.lua
+++ projectiles/multiplayerxppulse/multiplayerxppulse.lua
@@ -1,5 +1,5 @@
 function init()
-  if entity.damageTeam().type == "friendly" and entity.entityType() == "player" then
+  if entity.damageTeam().type == "friendly" and entity.entityType() == "player" and entity.id() ~= effect.sourceEntity() then
   	status.addPersistentEffect("ivrpgmultiplayerxp", {stat = "ivrpgmultiplayerxp", amount = math.floor(effect.duration())})
   end
   effect.expire()
--- scripts/ivrpgstatboosts.lua
+++ scripts/ivrpgstatboosts.lua
@@ -896,7 +896,7 @@ function updateXPPulse()
 			if status.statPositive("ivrpgmultiplayerxp") then
 				status.clearPersistentEffects("ivrpgmultiplayerxp")
 			else
-				world.spawnProjectile("multiplayerxppulse", mcontroller.position(), self.id, {0,0}, true, {power = 0, knockback = 0, timeToLive = 0.1, statusEffects = {{effect = "multiplayerxppulse", duration = world.entityCurrency(self.id, "experienceorb") - self.xp}}})
+				world.spawnProjectile("multiplayerxppulse", mcontroller.position(), self.id, {0,0}, true, {power = 0, knockback = 0, timeToLive = 0.1, statusEffects = {{effect = "multiplayerxppulse", duration = new}}})
 			end
 		end
 	end
