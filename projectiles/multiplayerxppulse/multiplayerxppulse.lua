5083d5109069463986720c2d05c8736eacadffa4
Roy <IcyVines@users.noreply.github.com>
2018-01-05 20:36:59 -0500

    XP Pulse

--- projectiles/multiplayerxppulse/multiplayerxppulse.lua
+++ projectiles/multiplayerxppulse/multiplayerxppulse.lua
@@ -1,7 +1,12 @@
 function init()
   --effect.setParentDirectives("border=3;e8981900;a36809")
   --self.damageProjectileType = config.getParameter("damageProjectileType") or "armorthornburst"
-  --sb.logInfo(projectile.getParameter("statusEffects")[1].effect)
+  sb.logInfo("Team: " .. entity.damageTeam().type .. ", Type: " .. entity.entityType())
+  if entity.damageTeam().type == "friendly" and entity.entityType() == "player" then
+  	sb.logInfo("Status: " .. effect.duration())
+  	status.addPersistentEffect("ivrpgmultiplayerxp", {stat = "ivrpgmultiplayerxp", amount = 1})
+  	world.spawnItem("experienceorb", mcontroller.position(), math.floor(effect.duration()))
+  end
 end
 
 function update(dt)
