a368ca388d0de2cb7edef78395008c2262955283
Roy <IcyVines@users.noreply.github.com>
2018-01-13 00:28:29 -0500

    Added Quest back in :(

---
+++ quests/ivrpgoneforall.lua
@@ -0,0 +1,10 @@
+require "/scripts/util.lua"
+
+function init()
+	quest.setFailureText("RPG Growth is phasing out the usage of this quest. Please do not worry that it has been failed.")
+	quest.fail()
+end
+
+function update(dt)
+	
+end
