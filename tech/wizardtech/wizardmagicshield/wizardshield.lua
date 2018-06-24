478d53617ce2db97b772c8c2ffe1f817b7097277
Roy <IcyVines@users.noreply.github.com>
2018-06-23 23:13:24 -0400

    Magic Shield pvp

--- tech/wizardtech/wizardmagicshield/wizardshield.lua
+++ tech/wizardtech/wizardmagicshield/wizardshield.lua
@@ -1,12 +1,13 @@
 function init()
   self.sourceId = effect.sourceEntity()
   self.id = entity.id()
+  self.statGroup = effect.addStatModifierGroup({})
 end
 
 
 function update(dt)
   if world.entityDamageTeam(self.id).type == "friendly" or (world.entityDamageTeam(self.id).type == "pvp" and not world.entityCanDamage(self.sourceId, self.id)) then
-    status.setPersistentEffects("ivrpgwizardshield", {
+    effect.setStatModifierGroup(self.statGroup, {
       {stat = "invulnerable", amount = 1},
       {stat = "lavaImmunity", amount = 1},
       {stat = "poisonStatusImmunity", amount = 1},
@@ -15,13 +16,15 @@ function update(dt)
       {stat = "waterImmunity", amount = 1},
       {stat = "fallDamageMultiplier", effectiveMultiplier = 0}
     })
+    sb.logInfo(world.entityDamageTeam(self.id).type)
+    sb.logInfo("on")
     effect.setParentDirectives("border=2;34ED2A20;4E1D7000")
   else
-    effect.setParentDirectives()
-    status.clearPersistentEffects("ivrpgwizardshield")
+    sb.logInfo("off")
+    effect.expire()
   end
 end
 
 function uninit()
-
+  effect.setParentDirectives()
 end
