ae4f6507246da38c0b2dfcb487aaa2769bfc0413
Roy <burningvines@gmail.com>
2021-11-18 01:11:04 -0500

    Began work on Orbital

---
+++ items/active/specweapons/warlock/controlprojectile.lua
@@ -0,0 +1,328 @@
+require "/scripts/vec2.lua"
+require "/scripts/util.lua"
+
+ControlProjectile = WeaponAbility:new()
+
+function ControlProjectile:init()
+  storage.projectiles = storage.projectiles or {}
+
+  self.elementalType = self.elementalType or self.weapon.elementalType
+
+  self.baseDamageFactor = config.getParameter("baseDamageFactor", 1.0)
+  self.stances = config.getParameter("stances")
+
+  activeItem.setCursor("/cursors/reticle0.cursor")
+  self.weapon:setStance(self.stances.idle)
+
+  -- Orbitals
+  self.projectileType = config.getParameter("projectileType")
+  self.projectileParameters = config.getParameter("projectileParameters")
+  self.projectileParameters.power = self.projectileParameters.power * root.evalFunction("weaponDamageLevelMultiplier", config.getParameter("level", 1))
+  self.cooldownTime = config.getParameter("cooldownTime", 0)
+  self.cooldownTimer = self.cooldownTime
+
+  storage.projectileIds = storage.projectileIds or {false, false, false}
+  self:checkProjectiles()
+  self.orbitRate = config.getParameter("orbitRate", 1) * -2 * math.pi
+  animator.resetTransformationGroup("orbs")
+  for i = 1, 3 do
+    animator.setAnimationState("orb"..i, storage.projectileIds[i] == false and "orb" or "hidden")
+  end
+  self:setOrbPosition(1)
+  -- End
+
+  self.weapon.onLeaveAbility = function()
+    self:reset()
+  end
+end
+
+function ControlProjectile:update(dt, fireMode, shiftHeld)
+  WeaponAbility.update(self, dt, fireMode, shiftHeld)
+
+  self:updateProjectiles()
+  self:checkProjectiles()
+
+  world.debugPoint(self:focusPosition(), "blue")
+
+  if (self.fireMode == (self.activatingFireMode or self.abilitySlot) and self.lastFireMode ~= "primary")
+    and not self.weapon.currentAbility
+    and not status.resourceLocked("energy") then
+
+    local nextOrbIndex = self:nextOrb()
+    if nextOrbIndex then
+      self.weapon:setStance(self.stances.discharge)
+      self:fire(nextOrbIndex)
+    end
+  end
+  self.lastFireMode = fireMode
+
+  animator.resetTransformationGroup("orbs")
+  animator.rotateTransformationGroup("orbs", -self.weapon.aimAngle or 0)
+  animator.translateTransformationGroup("orbs", {0, 2.5})
+  local check = false
+  for i = 1,3 do
+    animator.rotateTransformationGroup("orb"..i, self.orbitRate * dt)
+    animator.setAnimationState("orb"..i, storage.projectileIds[i] == false and "orb" or "hidden")
+    check = check or storage.projectileIds[i]
+  end
+  if not check then
+    self.weapon:setStance(self.stances.idle)
+  end
+end
+
+function ControlProjectile:charge()
+  self.weapon:setStance(self.stances.charge)
+
+  animator.playSound(self.elementalType.."charge")
+  animator.setAnimationState("charge", "charge")
+  animator.setParticleEmitterActive(self.elementalType .. "charge", true)
+  activeItem.setCursor("/cursors/charge2.cursor")
+
+  local chargeTimer = self.stances.charge.duration
+  while chargeTimer > 0 and self.fireMode == (self.activatingFireMode or self.abilitySlot) do
+    chargeTimer = chargeTimer - self.dt
+
+    mcontroller.controlModifiers({runningSuppressed=true})
+
+    coroutine.yield()
+  end
+
+  animator.stopAllSounds(self.elementalType.."charge")
+
+  if chargeTimer <= 0 then
+    self:setState(self.charged)
+  else
+    animator.playSound(self.elementalType.."discharge")
+    self:setState(self.cooldown)
+  end
+end
+
+function ControlProjectile:charged()
+  self.weapon:setStance(self.stances.charged)
+
+  animator.playSound(self.elementalType.."fullcharge")
+  animator.playSound(self.elementalType.."chargedloop", -1)
+  animator.setParticleEmitterActive(self.elementalType .. "charge", true)
+
+  local targetValid
+  while self.fireMode == (self.activatingFireMode or self.abilitySlot) do
+    targetValid = self:targetValid(activeItem.ownerAimPosition())
+    activeItem.setCursor(targetValid and "/cursors/chargeready.cursor" or "/cursors/chargeinvalid.cursor")
+
+    mcontroller.controlModifiers({runningSuppressed=true})
+
+    coroutine.yield()
+  end
+
+  self:setState(self.discharge)
+end
+
+function ControlProjectile:discharge()
+  self.weapon:setStance(self.stances.discharge)
+
+  activeItem.setCursor("/cursors/reticle0.cursor")
+
+  if self:targetValid(activeItem.ownerAimPosition()) and status.overConsumeResource("energy", self.energyCost * self.baseDamageFactor) then
+    animator.playSound(self.elementalType.."activate")
+    self:createProjectiles()
+  else
+    animator.playSound(self.elementalType.."discharge")
+    self:setState(self.cooldown)
+    return
+  end
+
+  util.wait(self.stances.discharge.duration, function(dt)
+    status.setResourcePercentage("energyRegenBlock", 1.0)
+  end)
+
+  while #storage.projectiles > 0 do
+    if self.fireMode == (self.activatingFireMode or self.abilitySlot) and self.lastFireMode ~= self.fireMode then
+      self:killProjectiles()
+    end
+    self.lastFireMode = self.fireMode
+
+    status.setResourcePercentage("energyRegenBlock", 1.0)
+    coroutine.yield()
+  end
+
+  animator.playSound(self.elementalType.."discharge")
+  animator.stopAllSounds(self.elementalType.."chargedloop")
+
+  self:setState(self.cooldown)
+end
+
+function ControlProjectile:cooldown()
+  self.weapon:setStance(self.stances.cooldown)
+  self.weapon.aimAngle = 0
+
+  animator.setAnimationState("charge", "discharge")
+  animator.setParticleEmitterActive(self.elementalType .. "charge", false)
+  activeItem.setCursor("/cursors/reticle0.cursor")
+
+  util.wait(self.stances.cooldown.duration, function()
+
+  end)
+end
+
+function ControlProjectile:targetValid(aimPos)
+  local focusPos = self:focusPosition()
+  return world.magnitude(focusPos, aimPos) <= self.maxCastRange
+      and not world.lineTileCollision(mcontroller.position(), focusPos)
+      and not world.lineTileCollision(focusPos, aimPos)
+end
+
+function ControlProjectile:createProjectiles()
+  local aimPosition = activeItem.ownerAimPosition()
+  local fireDirection = world.distance(aimPosition, self:focusPosition())[1] > 0 and 1 or -1
+  local pOffset = {fireDirection * (self.projectileDistance or 0), 0}
+  local basePos = activeItem.ownerAimPosition()
+
+  local pCount = self.projectileCount or 1
+
+  local pParams = copy(self.projectileParameters)
+  pParams.power = self.baseDamageFactor * pParams.baseDamage * config.getParameter("damageLevelMultiplier") / pCount
+  pParams.powerMultiplier = activeItem.ownerPowerMultiplier()
+
+  for i = 1, pCount do
+    local projectileId = world.spawnProjectile(
+        self.projectileType,
+        vec2.add(basePos, pOffset),
+        activeItem.ownerEntityId(),
+        pOffset,
+        false,
+        pParams
+      )
+
+    if projectileId then
+      table.insert(storage.projectiles, projectileId)
+      world.sendEntityMessage(projectileId, "updateProjectile", aimPosition)
+    end
+
+    pOffset = vec2.rotate(pOffset, (2 * math.pi) / pCount)
+  end
+end
+
+function ControlProjectile:focusPosition()
+  return vec2.add(mcontroller.position(), activeItem.handPosition(animator.partPoint("stone", "focalPoint")))
+end
+
+-- give all projectiles a new aim position and let those projectiles return one or
+-- more entity ids for projectiles we should now be tracking
+function ControlProjectile:updateProjectiles()
+  local aimPosition = activeItem.ownerAimPosition()
+  local newProjectiles = {}
+  for _, projectileId in pairs(storage.projectiles) do
+    if world.entityExists(projectileId) then
+      local projectileResponse = world.sendEntityMessage(projectileId, "updateProjectile", aimPosition)
+      if projectileResponse:finished() then
+        local newIds = projectileResponse:result()
+        if type(newIds) ~= "table" then
+          newIds = {newIds}
+        end
+        for _, newId in pairs(newIds) do
+          table.insert(newProjectiles, newId)
+        end
+      end
+    end
+  end
+  storage.projectiles = newProjectiles
+end
+
+function ControlProjectile:killProjectiles()
+  for _, projectileId in pairs(storage.projectiles) do
+    if world.entityExists(projectileId) then
+      world.sendEntityMessage(projectileId, "kill")
+    end
+  end
+end
+
+function ControlProjectile:reset()
+  self.weapon:setStance(self.stances.idle)
+  animator.stopAllSounds(self.elementalType.."chargedloop")
+  animator.stopAllSounds(self.elementalType.."fullcharge")
+  animator.setAnimationState("charge", "idle")
+  animator.setParticleEmitterActive(self.elementalType .. "charge", false)
+  activeItem.setCursor("/cursors/reticle0.cursor")
+end
+
+function ControlProjectile:uninit(weaponUninit)
+  self:reset()
+  if weaponUninit then
+    self:killProjectiles()
+  end
+end
+
+function ControlProjectile:nextOrb()
+  for i = 1, 3 do
+    if not storage.projectileIds[i] then
+      return i
+    end
+  end
+end
+
+function ControlProjectile:availableOrbCount()
+  local available = 0
+  for i = 1, 3 do
+    if not storage.projectileIds[i] then
+      available = available + 1
+    end
+  end
+  return available
+end
+
+function ControlProjectile:updateHand()
+  local isFrontHand = (activeItem.hand() == "primary") == (mcontroller.facingDirection() < 0)
+  animator.setGlobalTag("hand", isFrontHand and "front" or "back")
+  activeItem.setOutsideOfHand(isFrontHand)
+end
+
+function ControlProjectile:fire(orbIndex)
+  local params = copy(self.projectileParameters)
+  params.powerMultiplier = activeItem.ownerPowerMultiplier()
+  params.ownerAimPosition = activeItem.ownerAimPosition()
+  local firePos = self:orbFirePosition(orbIndex)
+  if world.lineCollision(mcontroller.position(), firePos) then return end
+  local projectileId = world.spawnProjectile(
+      self.projectileType,
+      self:orbFirePosition(orbIndex),
+      activeItem.ownerEntityId(),
+      self:orbAimVector(orbIndex),
+      false,
+      params
+    )
+  if projectileId then
+    storage.projectileIds[orbIndex] = projectileId
+    self.cooldownTimer = self.cooldownTime
+    animator.playSound("fire")
+  end
+end
+
+function ControlProjectile:orbFirePosition(orbIndex)
+  return vec2.add(mcontroller.position(), activeItem.handPosition(animator.partPoint("orb"..orbIndex, "orbPosition")))
+end
+
+function ControlProjectile:orbAimVector(orbIndex)
+  return vec2.norm(world.distance(activeItem.ownerAimPosition(), self:orbFirePosition(orbIndex)))
+end
+
+function ControlProjectile:checkProjectiles()
+  for i, projectileId in ipairs(storage.projectileIds) do
+    if projectileId and not world.entityExists(projectileId) then
+      storage.projectileIds[i] = false
+    end
+  end
+end
+
+function ControlProjectile:setOrbPosition(spaceFactor, distance)
+  for i = 1, 3 do
+    animator.resetTransformationGroup("orb"..i)
+    animator.translateTransformationGroup("orb"..i, {distance or 0, 0})
+    animator.rotateTransformationGroup("orb"..i, 2 * math.pi * spaceFactor * ((i - 2) / 3))
+  end
+end
+
+function ControlProjectile:setOrbAnimationState(newState)
+  for i = 1, 3 do
+    animator.setAnimationState("orb"..i, newState)
+  end
+end
